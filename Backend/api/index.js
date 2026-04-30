5d8b71d369b701d7acce50d716ed080322852c8a
sp3203814-ops <sp3203814@gmail.com>
2026-04-30 22:32:02 +0530

    backend ready hai

--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,3 +1,3 @@
-import app from "../backend/server.js";
+import app from "../server.js";
 
 export default app;
--- Backend/server.js
+++ Backend/server.js
@@ -14,32 +14,21 @@ connectDB();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-  })
-);
-app.use(async (req, res, next) => {
-  await connectDB();
-  next();
-});
 app.use(express.json());
 
-
-app.use("/api/auth", authRoutes);
-app.use("/api/tutor", tutorRoutes);
-app.use("/api/contact", contactRoutes);
-app.use("/api/enquiry", enquiryRoutes);
-app.use("/api/admin", adminRoutes);
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 
 app.get("/", (req, res) => {
   res.send("API Running...");
 });
 
-const PORT = process.env.PORT || 5000;
+app.use("/api/auth", authRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/enquiry", enquiryRoutes);
+app.use("/api/tutor", tutorRoutes);
+app.use("/api/admin", adminRoutes);
 
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
 export default app;
