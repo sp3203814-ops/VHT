b4c52c0dbf79defe88abdc5cb4077c6db735e15d
sp3203814-ops <sp3203814@gmail.com>
2026-04-30 09:50:22 +0530

    backend updated for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -36,6 +36,7 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+export default app;
